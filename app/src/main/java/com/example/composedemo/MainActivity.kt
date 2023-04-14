f66ba313ee276f590f3ad6360fa044442dfa1f92
joeysparidaans <joey.sparidaans@flitsmeister.nl>
2023-04-14 16:06:39 +0200

    persist state through configuration changes and process death

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,7 +33,8 @@ class MainActivity : ComponentActivity() {
 fun MyApp(
     modifier: Modifier = Modifier
 ) {
-    var shouldShowOnboarding by remember {
+    // Remember saveable makes a piece of state survive configuration changes and/or process death
+    var shouldShowOnboarding by rememberSaveable {
         mutableStateOf(true)
     }
     Surface(modifier = modifier) {
