6342b369b3e35302862c03277bad1b749eed2dd4
joeysparidaans <joey.sparidaans@flitsmeister.nl>
2023-04-14 14:45:11 +0200

    getting started comments

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -11,11 +11,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composedemo.ui.theme.ComposeDemoTheme
-
+// Activity is still the entry point for the app
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Entry point to Compose
         setContent {
+            // Themes are a way to style composable functions
             ComposeDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -29,11 +31,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// Composables are denoted with this annotation. It enables a function to call other @Composable functions within
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+// This annotation allows us to preview our composables in the IDE
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
